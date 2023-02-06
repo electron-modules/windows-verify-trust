5cf99f79ebf17e69b42ce9e2ce315adea98ef6cb
sriting <sriting@163.com>
2023-02-07 01:38:20 +0800

    feat: add verification of library certificate

--- bin/win-verify-trust.js
+++ bin/win-verify-trust.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
 'use strict';
+const pkg = require('../build/Release/win-verify-trust.node');
+
+function verify(name) {
+  const status = pkg.verifySignature(name);
+  const result = {};
+  if (status === 0) {
+    result.trusted = true;
+    result.message = "The library is signed, the signature was verified";
+  } else {
+    result.trusted = false;
+    result.message = `error: ${status}`;
+  }
+  return result;
+}
+
+console.log('C++ addon', verify("ntdll.dll"));
